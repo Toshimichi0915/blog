82d977eb5227c1998fc5bf9bbeb69a8873a5813d
Toshimichi0915 <main@toshimichi.net>
2023-08-16 04:34:26 +0900

    Add custom domain settings

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,6 @@
 import { SSTConfig } from "sst"
 import { Bucket, NextjsSite } from "sst/constructs"
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager"
 
 export default {
   config() {
@@ -15,6 +16,17 @@ export default {
         bind: [bucket],
         warm: 1,
         timeout: 60,
+        customDomain: {
+          isExternalDomain: true,
+          domainName: stack.stage === "prod" ? "blog.toshimichi.net" : "blog-dev.toshimichi.net",
+          cdk: {
+            certificate: Certificate.fromCertificateArn(
+              stack,
+              "toshimichi-cert",
+              "arn:aws:acm:us-east-1:581663711277:certificate/c39b8026-d3e4-47c9-9d58-3b5bad7a0781"
+            ),
+          },
+        },
       })
 
       stack.addOutputs({
