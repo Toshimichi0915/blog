224d47cd5ebc2ff3af78f8b70cfe508f106b806c
Toshimichi0915 <main@toshimichi.net>
2023-08-16 13:00:45 +0900

    Add environment settings

--- sst.config.ts
+++ sst.config.ts
@@ -27,6 +27,11 @@ export default {
             ),
           },
         },
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL ?? "",
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "",
+          NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? "",
+        }
       })
 
       stack.addOutputs({
